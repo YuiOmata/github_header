56b69a46c1db342630d1c0cbfd24dc608ec47663
YuiOmata <yuuri.o.drm@gmail.com>
2017-02-14 19:39:24 +0900

    chage version

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-
-var header = document.getElementsByClassName('header');
+//
+alert("ヘッダーを白くしようね")
+window.onload=function(){
+// var css=document.createElement(“link”);
+// var header = document.getElementsByClassName('header');
+  alert("ヘッダーを白くしようね")
+}
