8ed4bb18e5fc7d31f5eff17aa835f7327d4e65cf
YuiOmata <yuuri.o.drm@gmail.com>
2017-02-14 21:39:02 +0900

    ヘッダーが白くなるようにした

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-//
-alert("ヘッダーを白くしようね")
 window.onload=function(){
-// var css=document.createElement(“link”);
-// var header = document.getElementsByClassName('header');
-  alert("ヘッダーを白くしようね")
+  var header = document.getElementsByClassName('header');
+  header[0].setAttribute('style','background-color: #f0f0f0;border-bottom: 1px solid #eee;');
+
+  var header_search = document.getElementsByClassName('header-search-wrapper');
+  header_search[0].setAttribute('style','background-color: #fff');
+
+  var header_search_scope = document.getElementsByClassName('header-search-scope');
+  header_search_scope[0].setAttribute('style','color: #000; border-right-color: #eee;');
+
+  var header_search_input = document.getElementsByClassName('header-search-input');
+  header_search_input[0].setAttribute('style','color: #000;');
+
+  var header_nav_links = document.getElementsByClassName('header-nav-link');
+  for(index in header_nav_links){
+    var re = new RegExp("[0-9]+");
+    if( re.test(index) ){
+      header_nav_links[index].setAttribute('style','color: #000;');
+    }
+  }
 }
